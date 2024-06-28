e655140b49394df0884fb5a5fdbb3a4e6c1323f4
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-06-28 22:04:03 +0200

    run logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 8;
-    
+    public float jumpForce = 20;
+
+    private float runModificator = 1f;
     private Rigidbody2D rb;
 
 
@@ -18,7 +20,34 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        float moveX = Input.GetAxisRaw("Horizontal") * moveSpeed;
-        rb.velocity = new Vector2(moveX, rb.velocity.y);    
+        Move();
+        Run(1.7f);
+        Jump();
+    }
+
+    public void Move()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal") * moveSpeed * runModificator;
+        rb.velocity = new Vector2(moveX, rb.velocity.y);
+    }
+
+    public void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    public void Run(float modificator)
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            runModificator = modificator;
+        }
+        else
+        {
+            runModificator = 1f;
+        }
     }
 }
