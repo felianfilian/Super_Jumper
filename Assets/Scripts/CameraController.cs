809d4dc44453cf10f79cc86a519533372083b785
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-06-30 10:02:46 +0200

    freeze camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,15 +6,29 @@ public class CameraController : MonoBehaviour
 {
     public Transform target;
 
+    public bool freezeVertical;
+    public bool freezeHorizontal;
+
+    private Vector3 positionStore;
 
     void Start()
     {
-        
+        positionStore = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        CameraFreeze();
+    }
+
+    public void CameraFreeze()
+    {
+        if (freezeVertical)
+        {
+            transform.position = new Vector3(transform.position.x, positionStore.y, transform.position.z);
+        }
     }
 }
