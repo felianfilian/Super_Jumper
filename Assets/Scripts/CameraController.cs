71b6092ad7b276327c9d5f506c2bf5b693b455f1
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-06-30 16:35:05 +0200

    camera clamp update

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
+    
 
+    [Header("Position")]
     public bool freezeVertical;
     public bool freezeHorizontal;
+    public bool clampPosition;
+    public Transform clampMin;
+    public Transform clampMax;
 
+    public Camera cam;
     private Vector3 positionStore;
+    private float halfWidth;
+    private float halfHeight;
 
     void Start()
     {
+        cam = GetComponent<Camera>();
         positionStore = transform.position;
+
+        halfHeight = cam.orthographicSize;
+        halfWidth = halfHeight * cam.aspect;
     }
 
     // Update is called once per frame
@@ -21,7 +33,9 @@ public class CameraController : MonoBehaviour
     {
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
 
+        Clamping();
         CameraFreeze();
+        
     }
 
     public void CameraFreeze()
@@ -34,5 +48,20 @@ public class CameraController : MonoBehaviour
         {
             transform.position = new Vector3(positionStore.x, transform.position.y, transform.position.z);
         }
+    }
+
+    public void Clamping()
+    {
+        if (clampPosition) 
+        {
+            float positionX = Mathf.Clamp(transform.position.x, clampMin.position.x, clampMax.position.x);
+            float positionY = Mathf.Clamp(transform.position.y, clampMin.position.y, clampMax.position.y);
+            transform.position = new Vector3(
+            positionX,
+            positionY,
+            transform.position.z
+            );
+        }
+        
     }
 }
